5ea734e2cddffe15277d44d04a70ba348785f1ce
Olga <o.panova27@gmail.com>
2022-10-18 21:19:07 +0300

    Evaluate expression without if

--- property/migrations/0004_auto_20221012_1839.py
+++ property/migrations/0004_auto_20221012_1839.py
@@ -5,10 +5,7 @@ from django.db import migrations
 def set_value_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     for flat in Flat.objects.all():
-        if flat.construction_year >= 2015:
-            flat.new_building = True
-        else:
-            flat.new_building = False
+        flat.new_building = flat.construction_year >= 2015
         flat.save()
         
         
